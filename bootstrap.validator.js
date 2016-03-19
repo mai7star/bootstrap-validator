49cd95a1a4af421fafe6282347960d9a8f18c740
Hiroyuki Toda <mai7star@gmail.com>
2016-03-19 10:50:15 +0900

    インデントをスペースに変更。ローダを修正。

--- bootstrap.validator.js
+++ bootstrap.validator.js
@@ -4,142 +4,148 @@
  *
  */
 (function(root, factory){
-	if(typeof define === "function" && define.amd){
-		// AMD. Register as an anonymous module.
-		define(['jquery', 'underscore', 'bootstrap'], factory);
+    if(typeof define === "function" && define.amd){
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'underscore', 'bootstrap'], factory);
 
-	} else {
-		if(typeof root.jQuery === 'undefined') {
-			throw new Error('bootbox.deferred requires jQuery.');
-		}
-		if(typeof root._ === 'undefined') {
-			throw new Error('bootbox.deferred requires underscore.js.');
-		}
-		if(typeof root.jQuery().emulateTransitionEnd === 'undefined') {
-			throw new Error('bootbox.deferred requires Bootbox.');
-		}
+    } else if (typeof exports === "object") {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require("jquery"), require("underscore"));
 
-		// Browser globals (root is window)
-		root.validator = factory(root.jQuery, root._);
-	}
+    } else {
+        if(typeof root.jQuery === 'undefined') {
+            throw new Error('bootstrap validtor requires jQuery.');
+        }
+        if(typeof root._ === 'undefined') {
+            throw new Error('bootstrap validtor requires underscore.js.');
+        }
+        if(typeof root.jQuery().emulateTransitionEnd === 'undefined') {
+            throw new Error('bootstrap validtor requires Bootbox.');
+        }
+
+        // Browser globals (root is window)
+        root.validator = factory(root.jQuery, root._);
+    }
 
 }(this, function($, _){
-	'use strict';
+    'use strict';
 
-	var validator = function(form, rules, callback){
-		this.form = form;
-		this.rules = rules || [];
-		this.callback = callback || _.constant({});
-	};
+    var validator = function(form, rules, callback){
+        this.form = form;
+        this.rules = rules || [];
+        this.callback = callback || _.constant({});
+    };
 
-	validator.prototype = {
-		validate: function(){
-			this.deferred = $.Deferred();
-			this.clearError();
+    validator.prototype = {
+        validate: function(){
+            this.deferred = $.Deferred();
+            this.clearError();
 
-		 	return this.deferred.promise();
-		},
+            return this.deferred.promise();
+        },
 
-		filter1: function(){
-			return !_.isUndefined($(this).data('error'));
-		},
+        filter1: function(){
+            return !_.isUndefined($(this).data('error'));
+        },
 
-		selector: function(selector){
-			return this.form.find(selector);
-		},
+        selector: function(selector){
+            return this.form.find(selector);
+        },
 
-		clearError: function(){
-			this.form
-				.find('.form-group')
-				.each(function(){
-					$(this).removeClass('has-error');
-				});
+        clearError: function(){
+            this.form
+                .find('.form-group')
+                .each(function(){
+                    $(this).removeClass('has-error');
+                });
 
-			var ps = this.form
-				.find('*')
-				.filter(this.filter1)
-				.map(function(){
-					var d = $.Deferred();
+            var ps = this.form
+                .find('*')
+                .filter(this.filter1)
+                .map(function(){
+                    var d = $.Deferred();
 
-					$(this)
-						.one('hidden.bs.tooltip', d.resolve)
-						.tooltip('destroy')
-						.removeData('error');
+                    $(this)
+                        .one('hidden.bs.tooltip', d.resolve)
+                        .tooltip('destroy')
+                        .removeData('error');
 
-				 	return d.promise();
-				}).get();
+                    return d.promise();
+                }).get();
 
-			$.when
-				.apply(null, ps)
-				.then(this.validateRules.bind(this));
-		},
+            $.when
+                .apply(null, ps)
+                .then(this.validateRules.bind(this));
+        },
 
-		validateRules: function(){
-			var ds = _.chain(this.rules)
-				.invoke('call', null, this.selector.bind(this))
-				.filter()
-				.push(this.wait())
-				.value();
+        validateRules: function(){
+            var ds = _.chain(this.rules)
+                .invoke('call', null, this.selector.bind(this))
+                .filter()
+                .push(this.wait())
+                .value();
 
-			$.when
-				.apply(null, ds)
-				.always(this.showErrors.bind(this));
-		},
+            $.when
+                .apply(null, ds)
+                .always(this.showErrors.bind(this));
+        },
 
-		showErrors: function(){
-			var has_error = this.form
-				.find("*")
-				.filter(this.filter1)
-				.each(function(){
-					var $this = $(this);
+        showErrors: function(){
+            var has_error = this.form
+                .find("*")
+                .filter(this.filter1)
+                .each(function(){
+                    var $this = $(this);
 
-					$this.parents('.form-group:first').addClass('has-error');
+                    $this.parents('.form-group:first').addClass('has-error');
 
-					var option = $this.data('error');
-					if(_.isString(option)){
-						option = {
-							placement: 'top',
-							title: option
-						};
-					}
-					option.trigger = 'manual';
+                    var option = $this.data('error');
+                    if(_.isString(option)){
+                        option = {
+                            placement: 'top',
+                            title: option
+                        };
+                    }
+                    option.trigger = 'manual';
 
-					$this
-						.tooltip(option)
-						.tooltip('show');
+                    $this
+                        .tooltip(option)
+                        .tooltip('show');
 
-					var tooltipid = $this.attr('aria-describedby');
-					$('#' + tooltipid)
-						.click(function(){
-							$this.tooltip('hide');
-						});
-				})
-				.get()
-				.length > 0;
+                    var tooltipid = $this.attr('aria-describedby');
+                    $('#' + tooltipid)
+                        .click(function(){
+                            $this.tooltip('hide');
+                        });
+                })
+                .get()
+                .length > 0;
 
-			if(has_error){
-				this.deferred.reject();
-				return;
-			}
+            if(has_error){
+                this.deferred.reject();
+                return;
+            }
 
-			var result = this.callback(this.selector.bind(this));
-			this.deferred.resolve(result);
-		},
+            var result = this.callback(this.selector.bind(this));
+            this.deferred.resolve(result);
+        },
 
-		wait: function(){
-			return $.Deferred(function(){
-				setTimeout(this.resolve, 100);
-			}).promise();
-		}
-	};
+        wait: function(){
+            return $.Deferred(function(){
+                setTimeout(this.resolve, 100);
+            }).promise();
+        }
+    };
 
-	$.fn.validate = function(rules, callback){
-		if(this.prop('tagName') != 'FORM'){
-			throw new Error('only support form tag');
-		}
+    $.fn.validate = function(rules, callback){
+        if(this.prop('tagName') != 'FORM'){
+            throw new Error('only support form tag');
+        }
 
-		return new validator(this, rules, callback).validate();
-	}
+        return new validator(this, rules, callback).validate();
+    }
 
-	return validator;
+    return validator;
 }));
